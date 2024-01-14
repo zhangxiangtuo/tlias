00cc566badc75a7c3707e514b14f27c6841c1aa6
张项橐 <15903218805@163.com>
2024-01-14 15:21:45 +0800

    SpringBoot适配Knife4j、mybatis-plus、返回公共结果类

---
+++ src/main/java/com/zxt/TliasApplication.java
@@ -0,0 +1,15 @@
+package com.zxt;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@MapperScan("{com.zxt.mapper}")
+public class TliasApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TliasApplication.class, args);
+    }
+
+}
